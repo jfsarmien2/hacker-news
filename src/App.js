08bf27a21d11803c2591989aeb6bad1856acd944
John <john.sarmiento452@gmail.com>
2021-08-31 15:31:52 +0800

    remove unnecessary import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import SearchForm from './SearchForm'
 import Stories from './Stories'
 import Buttons from './Buttons'
-import { useGlobalContext } from './context'
 function App() {
   return (
        <>
--- src/reducer.js
+++ src/reducer.js
@@ -39,6 +39,7 @@ const reducer = (state, action) => {
           }
           return { ...state, page: prevPage }
         }
+        break;
     default:
       return state;
   }
